a6eafd83695807f4c50d8eb663b6aa0c11128083
andrewnova <tech.easy1@outlook.com>
2023-02-21 20:55:04 -0600

    api

--- src/test/java/api/tests/AirtableTests.java
+++ src/test/java/api/tests/AirtableTests.java
@@ -1,5 +1,8 @@
 package api.tests;
 
+import api.models.ResponseBody;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Test;
@@ -8,13 +11,20 @@ import utils.Config;
 public class AirtableTests {
 
     @Test
-    public void getRecords() {
+    public void getRecords() throws JsonProcessingException {
         Response response = RestAssured.given()
                 .header("Authorization", "Bearer keyUciDKN0atCXT7w")
                 .urlEncodingEnabled(false)
                 .get(Config.getProperty("baseUrl"));
 
         System.out.println(response.statusCode());
+
+        ObjectMapper obj = new ObjectMapper();
+
+        ResponseBody rb = obj.readValue(response.asString(), ResponseBody.class);
+
+        System.out.println(rb.getRecords().get(0).getFields().getFirstName());
+
 
     }
 }
