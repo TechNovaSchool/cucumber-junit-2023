7aa38fd8aeee1703ab27e0cb870a0a335d3bd9a4
andrewnova <tech.easy1@outlook.com>
2023-02-22 19:01:16 -0600

    api

--- src/test/java/api/tests/AirtableTests.java
+++ src/test/java/api/tests/AirtableTests.java
@@ -1,5 +1,6 @@
 package api.tests;
 
+import api.models.Record;
 import api.models.ResponseBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,23 +28,40 @@ public class AirtableTests {
 
         int size = rb.getRecords().size();
 
-        for (int i = 0; i < size; i++) {
-            if (rb.getRecords().get(i).getFields().getFirstName().startsWith("A")) {
-                String firstName = rb.getRecords().get(i).getFields().getFirstName();
-                String lastName = rb.getRecords().get(i).getFields().getLastName();
-                String email = rb.getRecords().get(i).getFields().getEmail();
-                boolean studentYes = rb.getRecords().get(i).getFields().isStudent();
-                int age = rb.getRecords().get(i).getFields().getAge();
+//        for (int i = 0; i < size; i++) {
+//            if (rb.getRecords().get(i).getFields().getFirstName().startsWith("A")) {
+//                String firstName = rb.getRecords().get(i).getFields().getFirstName();
+//                String lastName = rb.getRecords().get(i).getFields().getLastName();
+//                String email = rb.getRecords().get(i).getFields().getEmail();
+//                boolean studentYes = rb.getRecords().get(i).getFields().isStudent();
+//                int age = rb.getRecords().get(i).getFields().getAge();
+//
+//                System.out.println(firstName + " " + lastName + " " + email
+//                        + " " + studentYes + " " +age);
+//            }
+//        }
 
-                System.out.println(firstName + " " + lastName + " " + email
-                        + " " + studentYes + " " +age);
+//        for (Record element : rb.getRecords()) {
+//            if (element.getFields().getFirstName().startsWith("A")) {
+//                System.out.println(element.getFields().getFirstName()
+//                        + " " + element.getFields().getLastName()
+//                        + " " + element.getFields().getEmail()
+//                        + " " + element.getFields().isStudent()
+//                        + " " + element.getFields().getAge());
+//
+//            }
+//        }
+
+        for (Record element : rb.getRecords()) {
+
+            String firstChart = element.getFields().getFirstName().substring(0, 1);
+            if (firstChart.equals("A")) {
+                System.out.println(element.getFields().getFirstName()
+                        + " " + element.getFields().getLastName()
+                        + " " + element.getFields().getEmail()
+                        + " " + element.getFields().isStudent()
+                        + " " + element.getFields().getAge());
             }
         }
-
-
-
-
-
-
     }
 }
