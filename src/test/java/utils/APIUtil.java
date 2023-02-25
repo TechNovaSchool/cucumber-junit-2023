3a86fd05de280db3470dfd129b2a8b7fb039cd80
andrewnova <tech.easy1@outlook.com>
2023-02-25 13:44:12 -0600

    api util

--- src/test/java/utils/APIUtil.java
+++ src/test/java/utils/APIUtil.java
@@ -1,20 +1,25 @@
 package utils;
 
+import api.models.RequestBody;
 import api.models.ResponseBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class APIUtil {
     //CRUD operations
     //GET,POST, PATCH, DELETE
 
+    private static Response response;
+    private static ResponseBody responseBody;
+
     public static void hitGet(String resource) {
 
         String uri = Config.getProperty("host") + resource;
 
-        Response response = RestAssured.given()
+        response = RestAssured.given()
                 .header("Authorization", "Bearer " + Config.getProperty("apiKey"))
                 .urlEncodingEnabled(false)
                 .get(uri);
@@ -24,7 +29,36 @@ public class APIUtil {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
-            ResponseBody responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+            responseBody = objectMapper.readValue(response.asString(), ResponseBody.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void hitPost(String resource, RequestBody body) {
+
+        String uri = Config.getProperty("host") + resource;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String bodyJson = "";
+        try {
+            bodyJson = objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + Config.getProperty("apiKey"))
+                .urlEncodingEnabled(false)
+                .contentType(ContentType.JSON)
+                .body(bodyJson)
+                .post(uri);
+
+        System.out.println(response.statusCode());
+
+        try {
+            responseBody = objectMapper.readValue(response.asString(),ResponseBody.class)
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
