506fa2d8549ede1680b3475154cebe5135d907b9
andrewnova <tech.easy1@outlook.com>
2023-02-28 20:52:30 -0600

    api util

--- src/test/java/step_def/AirtableSteps.java
+++ src/test/java/step_def/AirtableSteps.java
@@ -1,12 +1,21 @@
 package step_def;
 
+import api.models.Myfields;
+import api.models.Record;
+import api.models.RequestBody;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import utils.APIUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AirtableSteps {
+    Faker faker = new Faker();
+    String recordID;
 
     String resource = "/Table%201";
 
@@ -33,8 +42,79 @@ public class AirtableSteps {
     public void userVerifyTheFirstName() {
 
         String actualName = APIUtil.getResponseBody().getFields().getFirstName();
-        Assert.assertEquals(actualName,"Elliot");
+        Assert.assertEquals(actualName, "Elliot");
 
 
+    }
+
+    @When("a user creates a record")
+    public void aUserCreatesARecord() {
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String fakeEmail = faker.internet().emailAddress();//myfakeemail@gmail.com
+        int index = fakeEmail.indexOf("@");
+        String emailProvider = fakeEmail.substring(index);
+
+        Myfields fields = new Myfields();
+        fields.setFirstName(firstName);
+        fields.setLastName(lastName);
+        fields.setAge(faker.number().numberBetween(16, 30));
+        fields.setEmail(firstName + "." + lastName + emailProvider);
+        fields.setStudent(faker.random().nextBoolean());
+        fields.setAddress(faker.address().streetAddress());
+
+        Record record = new Record();
+        record.setFields(fields);
+
+        List<Record> records = new ArrayList<>();
+        records.add(record);
+
+        RequestBody requestBody = new RequestBody();
+        requestBody.setRecords(records);
+
+        APIUtil.hitPost(resource, requestBody);
+
+    }
+
+    @When("a user is saving the recordID")
+    public void aUserIsSavingTheRecordID() {
+        recordID = APIUtil.getResponseBody().getRecords().get(0).getId();
+    }
+
+    @And("a user updates email")
+    public void aUserUpdatesEmail() {
+        Myfields fields = new Myfields();
+        fields.setEmail(faker.internet().emailAddress());
+
+        Record record = new Record();
+        record.setFields(fields);
+        record.setId(recordID);
+
+        List<Record> records = new ArrayList<>();
+        records.add(record);
+
+        RequestBody requestBody = new RequestBody();
+        requestBody.setRecords(records);
+
+        APIUtil.hitPatch(resource, requestBody);
+
+
+    }
+
+    @When("user delete the recordID")
+    public void userDeleteTheRecordID() {
+        APIUtil.hitDelete(resource, recordID);
+    }
+
+    @When("a user tries to create a record with invalid payload")
+    public void aUserTriesToCreateARecordWithInvalidPayload() {
+        RequestBody requestBody = new RequestBody();
+        APIUtil.hitPost(resource, requestBody);
+    }
+
+
+    @Then("user will receive status code: {int}")
+    public void userWillReceiveStatusCode(int statusCode) {
+        Assert.assertEquals(statusCode, APIUtil.getResponse().statusCode());
     }
 }
--- src/test/resources/features/airtableAPIs/airtableApi.feature
+++ src/test/resources/features/airtableAPIs/airtableApi.feature
@@ -10,4 +10,21 @@ Feature: Airtable API Tests
     Then user will receive status "200"
     And user verify the first name
 
+  @apiPostMethod
+  Scenario: Verify Post Endpoint
+    When a user creates a record
+    Then user will receive status "200"
+    When a user is saving the recordID
+    And a user updates email
+    Then user will receive status "200"
+    When user delete the recordID
+    Then user will receive status "200"
+
+    @apiNegativeTestExample
+    Scenario: Create a request with a wrong request body
+      When a user tries to create a record with invalid payload
+      Then user will receive status code: 422
+
+
+
 
